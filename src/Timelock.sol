d2ec2de2af8e42dc80491f649deb1695d6ce9b76
chamit6450 <chamit6450@gmail.com>
2025-05-07 11:02:01 +0530

    lock contract done

--- src/Timelock.sol
+++ src/Timelock.sol
@@ -1,22 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Timelock {
-    address public accountAddress;
-    uint256 public amount;
 
-    mapping(address => uint256) public lockedAmount;
-    // constructor(address _accountAddress, uint256 _amount){
-
-    // }
-
-    function lock(address _accountAddress, uint256 _amount) public{
-         
+contract Timelock{
+    struct Lock{
+     uint256 amount;
+     uint256 unlockTime;
     }
 
-    function withdraw(address _accountAddress, uint256 _amount) public{
-        require( lockedAmount[_accountAddress] >= _amount);
-       lockedAmount[_accountAddress] -= _amount;
-       transfer
+    mapping(address => Lock) public locked;
+    
+
+    function lock(uint256 _unlockTime) external payable{
+         require(msg.value > 0, "Send vaild ETH");
+         require(_unlockTime > block.timestamp, "unlock time cannot be less than current time");
+         locked[msg.sender].amount += msg.value;
+         locked[msg.sender].unlockTime += _unlockTime;
     }
+
+    function withdraw() external{
+       require( locked[msg.sender].amount > 0, "enter valid amount");
+       require(block.timestamp > locked[msg.sender].unlockTime, "Funds not matured yet");
+
+       uint256 userAmount = locked[msg.sender].amount;
+       locked[msg.sender].amount = 0;
+
+       payable(msg.sender).transfer(userAmount);
+    }
+    function getLockedBalance(address user) external view returns (uint256) {
+        return locked[user].amount;
+    }
+
+    receive() external payable {
+    revert("Use lock() to deposit ETH");
+   }
+
 }
